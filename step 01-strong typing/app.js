67a4b7650a4f940f600a3cffb7db9f04a592e25e
Areeba Ahmed khan <159721838+gulariba@users.noreply.github.com>
2024-06-25 23:00:13 +0500

    Add files via upload

---
+++ step 01-strong typing/app.js
@@ -0,0 +1,11 @@
+// strong typing
+// string, number , boolean, any
+// these are primitive data types
+var myName = "Areeba Ahmed Khan";
+console.log(myName);
+var countDown = 30;
+console.log(countDown);
+var idPassword = true;
+console.log(idPassword);
+var object = "apple";
+console.log(object);
---
+++ step 01-strong typing/app.ts
@@ -0,0 +1,19 @@
+// strong typing
+ // string, number , boolean, any
+ // these are primitive data types
+ 
+ let myName : string = "Areeba Ahmed Khan";
+ 
+ console.log(myName);
+
+let countDown :number = 30;
+
+console.log(countDown);
+
+let idPassword : boolean = true;
+
+console.log(idPassword);
+
+let object : any = "apple";
+
+console.log(object);
