af25cd2e8595547a19529b56c4837cb5152a3758
Areeba Ahmed khan <159721838+gulariba@users.noreply.github.com>
2024-06-26 20:52:19 +0500

    Add files via upload

---
+++ step 02 let--const/main.js
@@ -0,0 +1,12 @@
+//let and const are two relatively new concepts for variable 
+//declarations in JavaScript. As we mentioned earlier, let is similar
+// to var in some respects, but allows users to avoid some of the common 
+//“gotchas” that users run into in JavaScript.
+//const is an augmentation of let in that it prevents re-assignment
+// to a variable.
+//With TypeScript being an extension of JavaScript, the language 
+//naturally supports let and const.
+var myIdname = "Areeba Ahmed Khan";
+console.log(myIdname); // in let we change value my times and reassign value also
+var myaccountnumber = "012345678";
+console.log(myaccountnumber); // in const we not change vriable value 
